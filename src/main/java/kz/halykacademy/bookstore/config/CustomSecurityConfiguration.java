71e7f91859c0e4e1d03808cffa194423d39e3e7f
Kanapova Aida <kanapova.aida@gmail.com>
2022-08-17 04:21:53 +0600

    corrected permission for roles

--- src/main/java/kz/halykacademy/bookstore/config/CustomSecurityConfiguration.java
+++ src/main/java/kz/halykacademy/bookstore/config/CustomSecurityConfiguration.java
@@ -28,10 +28,27 @@ public class CustomSecurityConfiguration {
     public SecurityFilterChain customFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/api/v1/orders/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/api/v1/orders/**").authenticated()
-                .antMatchers(HttpMethod.POST,"api/v1/orders/updateOrderByAdmin").hasAuthority("ADMIN")
-                .antMatchers( "/api/v1/**").hasAuthority("ADMIN")
+
+                .antMatchers(HttpMethod.POST,"/api/v1/orders/addOrder").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/v1/orders/updateOrder").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/v1/orders/updateOrderByAdmin").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/api/v1/orders/**").authenticated()
+
+                .antMatchers(HttpMethod.PUT,"/api/v1/users/updateUserByUser").authenticated()
+                .antMatchers(HttpMethod.PUT,"/api/v1/users/updateUserByAdmin").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/v1/users/addUser").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/api/v1/users/**").hasAuthority("ADMIN")
+
+
+                .antMatchers("/api/v1/publishers/**").hasAuthority("ADMIN")
+
+                .antMatchers("/api/v1/authors/**").hasAuthority("ADMIN")
+
+                .antMatchers("/api/v1/genres/**").hasAuthority("ADMIN")
+
+                .antMatchers("/api/v1/books/**").hasAuthority("ADMIN")
+
+
                 .and().formLogin()
                 .and().httpBasic().authenticationEntryPoint(myBasicAuthenticationEntryPoint);
         return http.build();
